882b1718fa8cb4062ccc8ff4567ad9f1742deaf5
Sebaslc1077 <32956259+Sebaslc1077@users.noreply.github.com>
2018-02-28 23:59:42 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var respuestas = {
+  p1: 1,
+  p2: 2,
+  p3: 3,
+  p4: 1,
+  p5: 3,
+  p6: 2,
+  p7: 1,
+  p8: 3,
+  p9: "Aumenta su  tiempo de reacción",
+  p10: 1
+};
+function validate() {
+  var form = document.getElementById("form");
+  console.log(form.childNodes);
+}
